0830dac9dc79d7f50a26b6ab288014575984b52a
Martin Blom <martin.blom@onslip.com>
2024-08-27 13:03:14 +0200

    Non-working expect.extend example.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,25 @@
 import { test, expect } from '@playwright/test';
+import { expect as baseExpect, ExpectMatcherState, Locator, MatcherReturnType } from '@playwright/test';
+
+const dateExpect = baseExpect.extend({
+  async toHaveText(this: ExpectMatcherState, date: Date, expected: string | RegExp, options?: { ignoreCase?: boolean, timeout?: number }): Promise<MatcherReturnType> {
+    try {
+      if (date instanceof Date) {
+        if (expected instanceof RegExp) {
+          baseExpect(date.toISOString()).toMatch(expected);
+        } else {
+          baseExpect(date.toISOString()).toBe(expected);
+        }
+      } else {
+        baseExpect(date as Locator).toHaveText(expected, options);
+      }
+
+      return { pass: true, message: () => 'Date is correct' };
+    } catch (error: any) {
+      return { pass: false, message: () => error.message, expected, actual: error.matcherResult?.accept };
+    }
+  },
+});
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -15,4 +36,15 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('contain text', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await expect(page.locator('nav ul a')).toHaveCount(4);
+  await expect(page.locator('nav ul a')).toHaveText(['Node.js', 'Python', 'Java', '.NET']);
+});
+
+test('date text', async ({ page }) => {
+  await dateExpect(new Date()).toHaveText(/2024/);
 });
